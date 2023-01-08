3e929c46500b9abdbf236942346eec61d9dfe0d1
Palash.95 <palashmalakar103@gmail.com>
2023-01-08 22:55:11 +0530

    complete upto repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let result = string;
+    if (number === 0){
+        result = "";
+    }
+    else if(number < 0){
+        result = "ERROR"
+    }
+    else{
+        for (let i = 1; i < number; i++){
+            result += string;
+       }
+    }
+    
+    return result;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newArray1 = string.split(" ");
+    let newArray2 = newArray1.map(function(word){
+        return word.split("").reverse().join("");
+    })
+    let result = newArray2.reverse().join(" ");
+    return result;
 };
 
 // Do not edit below this line
