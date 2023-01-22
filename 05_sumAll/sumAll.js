dd7287d6f9bd99fd208791fc02c45997a27c9e05
Palash.95 <palashmalakar103@gmail.com>
2023-01-22 17:10:15 +0530

    complete removeFromArray and sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+    let total = 0;
+    if (firstNumber < 0 || !Number.isInteger(firstNumber) || secondNumber < 0 || !Number.isInteger(secondNumber)){
+        return "ERROR"
+    }
+    else if (firstNumber < secondNumber){
+        for (let i = firstNumber; i <= secondNumber; i++){
+            total += i;
+        }
+    }
+    else {
+        for (let i = secondNumber; i <= firstNumber; i++){
+            total += i;
+    }}
+    
+    return total;
 };
 
 // Do not edit below this line
